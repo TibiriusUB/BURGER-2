8b95a9398094fd9c527576aa6f5dd1378208cb86
Tibirius <tibirius@gmail.com>
2019-03-21 01:01:56 -0400

    Finalized

--- models/burger.js
+++ models/burger.js
@@ -15,32 +15,3 @@ module.exports = function (sequelize, DataTypes) {
     });
     return burger;
 };
-
-// import orm.js
-// var orm = require("../config/orm.js");
-// //call the orm
-
-// var burger = {
-//     all: function (callBack) {
-//         orm.selectAll("burgers", function (res) {
-//             callBack(res);
-//             //console.log(res);
-//         })
-//     },
-//     new: function (newBurger, callBack) {
-//         //console.log(newBurger)
-//         orm.insertOne("burgers", newBurger, function (res) {
-//             callBack(res);
-//             //console.log(res);
-//         })
-//     },
-//     eat: function (eaten, burger_id, callBack) {
-//         console.log(eaten, burger_id)
-//         orm.updateOne("burgers", eaten, burger_id, function (res) {
-//             callBack(res);
-//             // console.log(res);
-//         })
-//     }
-// };
-// // export this stuff
-// module.exports = burger;
